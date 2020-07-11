c44ef211e687c035ccfea97270f0e27acc235d20
Fabien Prudhomme <fabien@deciderata.fr>
2020-07-11 15:27:00 +0200

    Tous propre

--- install.sh
+++ install.sh
@@ -2,57 +2,66 @@
 
 set -e
 
-readonly COMPOSE_INSTALL=1
-readonly COMPOSE_VERSION='1.26.2'
+originalPath=$(pwd)
 
-#uninstall old version
-sudo apt-get -y remove docker docker-engine docker.io containerd runc
-
-sudo apt-get -y remove \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-
-sudo apt-get -y remove docker-ce docker-ce-cli containerd.io
+. $originalPath/conf/function.sh
 
 
-sudo apt-get update
+# configure .env
+for line in $(cat ./conf/.env.dist | grep -vE '^#')
+do
+    key=$(echo ${line} | awk -F "=" '{print $1}')
+    configureEnv ${key} $(getEnvValue ${key})
+done
 
+displaybegin "Update du système"
+sudo apt-get update > log 2>&1
+displayEnd "Update du système"
+
+displaybegin "Install common tools"
 sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common > log 2>&1
+displayEnd "Install common tools"
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+displaybegin "Ajout du repository"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - > log 2>&1
 
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
-   stable"
+   stable" > log 2>&1
+displayEnd "Ajout du repository"
 
-sudo apt-get -y update
-
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-
-echo "Ajout du groupe"
-#sudo groupadd -f docker
-
-echo "Changement de droit socket"
-#sudo chown root:docker /var/run/docker.sock
-
-echo "Ajout de l'utilisateur $(whoami) au groupe docker"
-#sudo adduser $(whoami) docker
-
-echo "restart service"
-#sudo systemctl restart docker
-
-#docker --version
-
-if [ "${COMPOSE_INSTALL}" == 1 ]; then
-    sudo curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
+if [ $(getEnvValue 'FRESH_INSTALL') != "yes" ]
+then
+    displaybegin "Remove old package"
+    sudo apt-get -y remove docker docker-engine docker.io containerd runc > log 2>&1
+    sudo apt-get -y remove docker-ce docker-ce-cli containerd.io > log 2>&1
+    displayEnd "Remove old package"
 fi
+
+displaybegin "Install docker"
+sudo apt-get -y update > log 2>&1
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io > log 2>&1
+displayEnd "Install docker"
+
+COMPOSE_INSTALL=$(getEnvValue 'COMPOSE_INSTALL')
+COMPOSE_VERSION=$(getEnvValue 'COMPOSE_VERSION')
+
+
+if [ "${COMPOSE_INSTALL}" == yes ]; then
+    displaybegin "Install docker compose"
+    sudo curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose  > log 2>&1
+    sudo chmod +x /usr/local/bin/docker-compose  > log 2>&1
+    displayEnd "Install docker compose"
+fi
+
+displaybegin "post config"
+sed "s/^FRESH_INSTALL=.*$/FRESH_INSTALL=no/" -i .env
+sed "s/^COMPOSE_INSTALL=.*$//" -i .env
+displayEnd "post config"
